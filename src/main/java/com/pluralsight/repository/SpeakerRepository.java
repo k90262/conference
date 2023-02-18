fd2bdac40d960ce14be39a1a23635d65558c6ae7
Bill Ho <ycho.bill.lab@gmail.com>
2023-02-19 00:01:46 +0800

    finish init and created a application to run

--- src/main/java/com/pluralsight/repository/SpeakerRepository.java
+++ src/main/java/com/pluralsight/repository/SpeakerRepository.java
@@ -1,2 +1,9 @@
-package com.pluralsight.repository;public interface SpeakerRepository {
+package com.pluralsight.repository;
+
+import com.pluralsight.model.Speaker;
+
+import java.util.List;
+
+public interface SpeakerRepository {
+    List<Speaker> findAll();
 }
