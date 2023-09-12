86bb39a2fe13348ad599bcd6639516a6de5ac89f
Bill Ho <ycho.bill.lab@gmail.com>
2023-09-12 22:03:17 +0800

    [ch07][Advanced Bean Configuration] SpEL

--- conference-java/src/main/java/Application.java
+++ conference-java/src/main/java/Application.java
@@ -10,11 +10,12 @@ public class Application {
         //SpeakerService service = new SpeakerServiceImpl();
 
         SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
-        System.out.println(service);
+        //System.out.println(service);
 
         System.out.println(service.findAll().get(0).getFirstName());
+        System.out.println(service.findAll().get(0).getSeedNum());
 
-        SpeakerService service2 = appContext.getBean("speakerService", SpeakerService.class);
-        System.out.println(service2);
+        //SpeakerService service2 = appContext.getBean("speakerService", SpeakerService.class);
+        //System.out.println(service2);
     }
 }
--- conference-java/src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ conference-java/src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.pluralsight.repository;
 import com.pluralsight.model.Speaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -15,6 +16,9 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
     @Autowired
     private Calendar cal;
 
+    @Value("#{ T(java.lang.Math).random() * 100 }")
+    private double seedNum;
+
     @Override
     public List<Speaker> findAll() {
         List<Speaker> speakers = new ArrayList<Speaker>();
@@ -23,6 +27,7 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
 
         speaker.setFirstName("Bryan");
         speaker.setLastName("Hansen");
+        speaker.setSeedNum(seedNum);
 
         System.out.println("cal: " + cal.getTime());
 
